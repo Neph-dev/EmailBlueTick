368956d7f38f70c99631b179ef52650cf209a1b9
Nephthali Salam <83629026+Neph-dev@users.noreply.github.com>
2023-09-30 11:12:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# EmailBlueTick
+ This is the small script that I wrote and use to track if my emails have been read.
---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+
+let emailTrackingData = []
+const trackingDataFilePath = 'trackingData.json'
+
+if (fs.existsSync(trackingDataFilePath)) {
+    const existingData = fs.readFileSync(trackingDataFilePath, 'utf8');
+    emailTrackingData = JSON.parse(existingData);
+}
+
+app.get('/image.png', (req, res) => {
+    const clientIP = req.ip;
+    const emailId = req.query.emailId;
+
+    emailTrackingData.push({
+        emailId,
+        timestamp: new Date(),
+        clientIP,
+    });
+
+    const trackingDataJSON = JSON.stringify(emailTrackingData, null, 2)
+    fs.writeFile(trackingDataFilePath, trackingDataJSON, (err) => {
+        if (err) {
+            console.error('Error writing tracking data:', err);
+        }
+    })
+    res.sendFile(__dirname + '/image.png')
+})
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
